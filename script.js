c0bc47df8bd8eee7e5a82db8507e74cfe714ed54
froggy-Master <menaschap@gmail.com>
2023-03-30 23:15:46 -0400

    Refactor JavaScript logic

--- script.js
+++ script.js
@@ -1,127 +1,147 @@
-let currentNumber = '';
-let currentEquation = '';
+//  The goal of this application is to perform basic calculator
+//  arithmatic tasks.  It can only perform simple eqautions—that
+//  is, inputs with two eqautions.  This resolves the difficulty 
+//  of handling the login of order of operation.
+//
+//  If you read the equation left-to-right, it would go like this:
+//  bottomRowNumber <operation> topRowNumber = result.
+let bottomRowNumber = '';
+let topRowNumber = '';
 
-function setCurrentNumber() {
-    if (parseInt(this.textContent) || this.textContent === '0' || (this.textContent === '.' && !currentNumber.includes('.'))) {
-        currentNumber += this.textContent;
+const topRow = document.querySelector('.top-row');
+const bottomRow = document.querySelector('.bottom-row');
+
+const renderBottomRow = () => {
+    if (bottomRowNumber.length > 10) {
+        bottomRowNumber = bottomRowNumber.substring(0, 10);
     }
-    setBottomRow();
-};
-
-function setCurrentEquation(value) {
-    currentEquation && operate();
-
-    if (currentNumber) {
-        currentEquation = currentNumber;
-        currentEquation += ` ${value}`;
-        setTopRow();
-        currentNumber = '';
-        setBottomRow();
-    }
+    bottomRow.textContent = bottomRowNumber;
 }
 
-function setBottomRow() {
-    document.querySelector('.bottom-row').textContent = currentNumber;
+const renderTopRow = () => {
+    topRow.textContent = topRowNumber;
 }
 
-function setTopRow() {
-    document.querySelector('.top-row').textContent = currentEquation;
+const createEquation = (symbol) => {
+    topRowNumber = `${bottomRowNumber} ${symbol}`;
+    renderTopRow();
+    bottomRowNumber = ' ';
+    renderBottomRow();
 }
 
-function clear() {
-    currentNumber = '';
-    currentEquation = '';
-    setTopRow();
-    setBottomRow();
+const clear = () => {
+    topRowNumber = '';
+    bottomRowNumber = '';
+    renderTopRow();
+    renderBottomRow();
 }
 
-function dlt() {
-    currentNumber = currentNumber.split('').slice(0, -1).join('');
-    setBottomRow();
+const dlt = () => {
+    const deletedNum = bottomRowNumber.split('').slice(0, -1).join('');
+    bottomRowNumber = deletedNum;
+    renderBottomRow();
 }
 
-function operate() {
-    if (currentEquation.split(' ').length > 1) {
-
-        const operant = currentEquation.split(' ')[0];
-
-        switch (currentEquation.split(' ')[1]) {
-            case '+':
-                currentNumber = (Math.floor((parseFloat(operant) + parseFloat(currentNumber)) * 1000000) / 1000000).toString();
-                break;
-            case '-':
-                currentNumber = (Math.floor((parseFloat(operant) - parseFloat(currentNumber)) * 1000000) / 1000000).toString();
-                break;
-            case 'x':
-                currentNumber = (Math.floor((parseFloat(operant) * parseFloat(currentNumber)) * 1000000) / 1000000).toString();
-                break;
-            case '÷':
-                currentNumber = (Math.floor((parseFloat(operant) / parseFloat(currentNumber)) * 1000000) / 1000000).toString();
-                break;
-        }
+const operate = (numberOne, numberTwo, operant) => {
+    switch (operant) {
+        case '+':
+            return (numberOne + numberTwo).toString();
+            break;
+        case '-':
+            return (numberOne - numberTwo).toString();
+            break;
+        case 'x':
+            return (numberOne * numberTwo).toString();
+            break;
+        case '÷':
+            return (numberOne / numberTwo).toString();
+            break;
     }
 }
 
-function handleKeyboardInput(e) {
+const handleEquals = e => {
+    if (!topRowNumber) {
+        clear();
+    } else {
+        const valueOne = parseFloat(topRowNumber.split(' ')[0]);
+        const valueTwo = parseFloat(bottomRowNumber);
+        const operator = topRowNumber.split(' ')[1];
+        const result = operate(valueOne, valueTwo, operator);
+        bottomRowNumber = result;
+        renderBottomRow();
+        topRowNumber = '';
+        renderTopRow();
+    }
+    
+}
+
+const overideOrderOfOperations = () => {
+    const valueOne = parseFloat(topRowNumber.split(' ')[0]);
+    const valueTwo = parseFloat(bottomRowNumber);
+    const operator = topRowNumber.split(' ')[1];
+    const result = operate(valueOne, valueTwo, operator);
+    bottomRowNumber = result.toString();
+    if (bottomRowNumber.length > 10) {
+        bottomRowNumber = bottomRowNumber.substring(0, 10)
+    }
+}
+
+const handleKeyboardInput = e => {
     const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const splitUp = e.code.split('');
     const lastDigit = splitUp[splitUp.length -1];
     if (e.code === 'Digit8' && e.shiftKey) {
-        setCurrentEquation('x');
+        topRowNumber && overideOrderOfOperations();
+        bottomRowNumber && createEquation('x');
     } else if (digits.includes(lastDigit)) {
-        currentNumber += lastDigit;
-        setBottomRow();
+        bottomRowNumber += lastDigit;
+        renderBottomRow();
     } else if (e.code === 'Period') {
-        currentNumber += '.';
-        setBottomRow()
+        bottomRowNumber += '.';
+        bottomRowNumber && renderBottomRow()
     } else if (e.code === 'Backspace') {
         dlt();
     } else if (e.code === 'Equal' && e.shiftKey) {
-        setCurrentEquation('+');
+        topRowNumber && overideOrderOfOperations();
+        bottomRowNumber && createEquation('+');
     } else if (e.code === 'Minus') {
-        setCurrentEquation('-');
+        topRowNumber && overideOrderOfOperations();
+        bottomRowNumber && createEquation('-');
     } else if (e.code === 'Slash') {
-        setCurrentEquation('÷')
+        topRowNumber && overideOrderOfOperations();
+        bottomRowNumber && createEquation('÷');
     } else if (e.code === 'Enter') {
-        operate();
-        setBottomRow()
-        currentEquation = '';
-        setTopRow();
+        handleEquals();
     }
 }
-   
-
-    
 
 
 
 
 
 
-
-
-
-
-
-
-window.addEventListener('keydown', handleKeyboardInput)
-
-document.getElementById('operatorEquals').addEventListener('click', () => {
-    operate();
-    currentEquation = '';
-    setTopRow();
-})
-
-document.querySelectorAll('.key').forEach(key => {
-    key.addEventListener('click', setCurrentNumber);
+document.querySelectorAll('.num').forEach(num => {
+    num.addEventListener('click', e => {
+        if (bottomRowNumber.length < 12 && parseInt(e.target.textContent) || e.target.textContent === '0' || (e.target.textContent === '.' && !bottomRowNumber.includes('.'))) {
+            bottomRowNumber += e.target.textContent;
+        }
+        renderBottomRow();
+    })
 })
 
 document.querySelectorAll('.operator').forEach(operator => {
-    operator.addEventListener('click', (e) => {
-        setCurrentEquation(e.target.textContent)
-    });
+    operator.addEventListener('click', e => {
+        if (topRowNumber) {
+            overideOrderOfOperations();
+        }
+        bottomRowNumber && createEquation(e.target.textContent);
+    })
 })
+
+document.querySelector('#operatorEquals').addEventListener('click', handleEquals);
 
 document.querySelector('#clear').addEventListener('click', clear);
 
-document.querySelector('#delete').addEventListener('click', dlt)
+document.querySelector('#delete').addEventListener('click', dlt);
+
+window.addEventListener('keydown', handleKeyboardInput)
